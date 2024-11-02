8f72be363312a0bfa4c24a467c87a969455b5478
Gjagadish123 <jagadishjagadish2022@gmail.com>
2024-11-02 15:53:39 +0530

    minor

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,8 +3,8 @@ export const environment = {
   //connect_url: 'https://teja-dev.springlogix.com/api/',
   //root_url: 'https://teja-dev.springlogix.com/'
 
-  connect_url: 'https://at-api.springlogix.com/api/',
-  root_url: 'https://at-api.springlogix.com/',
+  connect_url: 'https://dev.springlogix.com/api/',
+  root_url: 'https://dev.springlogix.com/',
 
   // connect_url: 'https://dev.springlogix.com/api/',
   // root_url: 'https://dev.springlogix.com/',
